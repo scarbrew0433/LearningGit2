3d7e9a5fcb3eb60ec8d43fcf109f2e00216ff21a
John Scarbrough <jpscarbrough@fedex.com>
2017-03-28 13:44:06 -0400

    added a stupid syso statement

--- JohnsGitTesting/src/com/fedex/jps/LearningGit.java
+++ JohnsGitTesting/src/com/fedex/jps/LearningGit.java
@@ -5,6 +5,8 @@ public class LearningGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lets learn Git");
+		
+		System.out.println("This is just a stupid output");
 	}
 
 }
