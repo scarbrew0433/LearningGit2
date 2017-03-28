1c832d1bb5f45f433a589004b5fc5958c9fe88d9
John Scarbrough <jpscarbrough@fedex.com>
2017-03-28 15:00:21 -0400

    initial GitMain2

---
+++ 2-LearningGit/src/com/fedex/jps/LearningGitMain2.java
@@ -0,0 +1,11 @@
+package com.fedex.jps;
+
+public class LearningGitMain2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Stupid output 1");
+		System.out.println("Line 2");
+	}
+
+}
