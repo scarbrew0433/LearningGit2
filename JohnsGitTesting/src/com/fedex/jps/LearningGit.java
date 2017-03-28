21f5f4ec6e73a29a887aee0986a4021062f4181b
John Scarbrough <jpscarbrough@fedex.com>
2017-03-28 13:34:37 -0400

    intitqal check in for teswting

---
+++ JohnsGitTesting/src/com/fedex/jps/LearningGit.java
@@ -0,0 +1,10 @@
+package com.fedex.jps;
+
+public class LearningGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lets learn Git");
+	}
+
+}
